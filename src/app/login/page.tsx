2281e943ba48ce4f09daea103fae8de07d19d60a
Hernan Tapia <soliditize@MacBook-Pro.local>
2024-10-03 16:11:47 -0700

    completed login and sign up

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,47 @@
 "use client";
 import Link from "next/link"
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { axios } from "axios";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function loginPage(){
+    const router = useRouter();
     const [user , setUser] = React.useState({
       email: "",
       password: "",
     })
 
+    const [buttonDisabled, setButtonDisable] = React.useState(false)
+    const [loading, setLoading] = React.useState(false)
+
     const onLogin = async () => {
-      
+      try {
+        setLoading(true)
+        const response = await axios.post("/api/users/login", user)
+        console.log("Login Sucess" , response.data)
+        router.push("/profile")
+        
+      } catch (error: any) {
+        console.log("Login Failed" , error.message)
+        toast.error(error.message)
+      } finally {
+        setLoading(false)
+      }
     }
 
+    useEffect(() => {
+      if(user.email.length > 0 && user.password.length >0 ){ 
+          setButtonDisable(false)
+         }
+      else{
+        setButtonDisable(true)
+      }
+    }, [user])
+
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 text-green-600" >
       <label htmlFor="email">email</label>
       <input className="p-2"
         id="email"
