f4a9a2d6b69cc4ec8893fa8a2c0e2b6d646e9ed6
Hernan Tapia <tapiahernan5@gmail.com>
2024-10-12 22:18:08 -0700

    located bug for verification, need to fix still

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -27,15 +27,15 @@ export const sendEmail = async ({email, emailType, userId } : any ) => {
       }
     });
 
+      // need to fix bugs 
     const mailOptions = {
       from: "fake@gmail.com",
       to: email,
       subject: emailType === "VERIFY" ? "Verify your email" : 
       "Reset your password",
-      html: `<p>click <a href="${process.env.DOMAIN}/ verifyemail?token=${hashedToken}" 
-      >here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"} 
-      or copy and paste the link below in your browser. <br> ${process.env.DOMAIN}/verifyemail?
-      token=${hashedToken} <p/> `
+      html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
+            or copy and paste the link below in your browser. <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
+            </p>`
     }
 
     const mailResponse = await transport.sendMail(mailOptions);
