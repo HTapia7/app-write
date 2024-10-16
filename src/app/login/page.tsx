ca79b375e81bfc04c5b4f1ab1c00ab2e144eb157
Hernan Tapia <tapiahernan5@gmail.com>
2024-10-15 23:17:04 -0700

    marked some files that need adjustment

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 import axios from "axios";
 import toast from "react-hot-toast";
 
-// Testing 
+// Testing  need to fix
 export default function LoginPage(){
     const router = useRouter();
     const [user , setUser] = React.useState({
